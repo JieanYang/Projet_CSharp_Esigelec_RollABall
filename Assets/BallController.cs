4e41bff62bbfdda79a3565d36a9d66f630393711
DI CUI <cuidi.fr@gmail.com>
2018-05-22 15:14:44 +0200

    Add mark

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BallController : MonoBehaviour {
 
     public float speed = 15;
-
+    public Text countText;
+    public Text winText;
     private Rigidbody rb;
+    private int count;
 
-    void Start()
+    void Start ()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     void FixedUpdate ()
@@ -23,11 +29,21 @@ public class BallController : MonoBehaviour {
         rb.AddForce(movement * speed);
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter (Collider other)
     {
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+    
+    void SetCountText(){
+        countText.text = "Count: " + count.ToString();
+        if (count >= 5)
+        {
+            winText.text = "You Win!";
         }
     }
 }
