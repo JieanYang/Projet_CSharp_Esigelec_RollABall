2dfc1ffad432e706ab6bf90b66d846d4e3d52bc7
DI CUI <cuidi.fr@gmail.com>
2018-05-23 15:04:49 +0200

    Add login/register page

---
+++ Assets/Scripts/register.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Text.RegularExpressions;
+
+
+public class register : MonoBehaviour {
+    
+    public GameObject username;
+    public GameObject password;
+    private string Username;
+    private string Password;
+    private string form;
+    private bool UserExtist = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	public void RegisterButton () {
+        if(Username == ""){
+            Debug.LogWarning("Username empty");
+        }
+        
+        
+        if(Password == ""){
+            Debug.LogWarning("Password empty");
+        }
+        
+    }
+    
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Tab)){
+            if (username.GetComponent<InputField>().isFocused){
+                password.GetComponent<InputField>().Select();
+            }
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Return)){
+            if (Password != "" && Username != ""){
+                RegisterButton ();
+            }
+        }
+		Username = username.GetComponent<InputField>().text;
+        Password = password.GetComponent<InputField>().text;
+	}
+}
