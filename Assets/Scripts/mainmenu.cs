91ded1444af0b4a7282358166e978018fdb62ba5
DICUI <cuidi.fr@gmail.com>
2018-05-24 16:04:23 +0200

    Add top3 list

---
+++ Assets/Scripts/mainmenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenu : MonoBehaviour {
+    public static string username = "Unknown";
+	// Use this for initialization
+	void Start () {
+		
+	}
+    public void PlayGame (){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+	public void QuitGame (){
+        Application.Quit();
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
