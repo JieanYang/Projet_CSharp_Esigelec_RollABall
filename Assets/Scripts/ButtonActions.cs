9e1160537fb7a1a074174321504618643b3a266f
JieanYang <yja8534786@gmail.com>
2018-05-26 23:59:19 +0200

    Optimise the code with buttons;

---
+++ Assets/Scripts/ButtonActions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonActions : MonoBehaviour {
+
+    // Next level button
+    public void NextLevel()
+    {
+        if (BallController.result_level1 == (System.DateTime.Now - System.DateTime.Now))
+        {
+            Debug.LogWarning("Username empty");
+        }
+        else
+        {
+            print("button next level");
+            SceneManager.LoadScene("Labyrinth");
+        }
+
+    }
+
+    // Back button
+    public void BackMenu()
+    {
+        SceneManager.LoadScene("menu");
+    }
+
+
+}
